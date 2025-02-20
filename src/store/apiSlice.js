d2b74c8b041030ef1771716d7a05fc1ca95d451b
varunkumar <varunkumar02021@gmail.com>
2025-02-20 16:27:32 +0530

    i change to render url

--- src/store/apiSlice.js
+++ src/store/apiSlice.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "http://localhost:3011",
+  baseUrl: "https://audible-backend-3.onrender.com",
   prepareHeaders: (headers, { getState }) => {
     const state = getState();
 
