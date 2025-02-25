17c2a9adfd4eee350336413d84d275ca67a8b58b
varunkumar <varunkumar02021@gmail.com>
2025-02-25 10:30:55 +0530

    I add user id to check token

--- src/store/apiSlice.js
+++ src/store/apiSlice.js
@@ -1,7 +1,8 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "https://audible-backend-3.onrender.com",
+  // baseUrl: "https://audible-backend-3.onrender.com",
+  baseUrl: "http://localhost:3013",
   prepareHeaders: (headers, { getState }) => {
     const state = getState();
 
